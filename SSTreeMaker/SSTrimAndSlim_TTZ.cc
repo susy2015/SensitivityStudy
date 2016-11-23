134599659078951dda3d45d41976a4c0a06ed51a
hua wei <hua.wei@cern.ch>
2016-11-23 13:52:49 -0600

    baseline code involved into code

--- SSTreeMaker/SSTrimAndSlim_TTZ.cc
+++ SSTreeMaker/SSTrimAndSlim_TTZ.cc
@@ -16,6 +16,28 @@
 #include "SusyAnaTools/Tools/searchBins.h"
 
 #include "SSTrimAndSlimCommon.h"
+
+inline size_t find_Nth
+(
+  const std::string & str ,   // where to work
+  unsigned            N ,     // N'th ocurrence
+  const std::string & find    // what to 'find'
+)
+{
+  if ( 0==N ) { return std::string::npos; }
+  size_t pos,from=0;
+  unsigned i=0;
+  while ( i<N )
+  {
+    pos=str.find(find,from);
+    if ( std::string::npos == pos ) { break; }
+    from = pos + 1; // from = pos + find.size();
+    ++i;
+  }
+  return pos;
+}
+
+//#include "SSTrimAndSlimCommon.h"
 // ===  FUNCTION  ============================================================
 //         Name:  GetGenChilds
 //  Description:  
