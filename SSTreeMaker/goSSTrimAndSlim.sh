8a81049638488dc3f9ff1dbb9002f9845ba0db4e
hua wei <hua.wei@cern.ch>
2016-11-25 00:34:37 -0600

    prev0, before move to zhenbin's interface change on baseline vessel and joe's update Supplmental ak8 additions

--- SSTreeMaker/goSSTrimAndSlim.sh
+++ SSTreeMaker/goSSTrimAndSlim.sh
@@ -2,12 +2,12 @@
 
 export PATH=${PATH}:/cvmfs/cms.cern.ch/common
 export CMS_PATH=/cvmfs/cms.cern.ch
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$1/src/SensitivityStudy/SSTreeMaker/obj:$1/src/TopTagger/TopTagger/test:$1/src/opencv/lib
 
 cd $1/src
 eval `scramv1 runtime -sh`
 
 cd ${_CONDOR_SCRATCH_DIR}
-
 python NTuple_SSTrimAndSlim.py $2 $3
 
 #find . -name "*.root" -exec xrdcp {} "root://cmseos.fnal.gov//store/group/lpcsusyhad/hua/Skimmed_2015Nov15/" \;
